a21d11e701c5e47fc0380f6c264b96db9406c9fd
jorge.reales@dialapplet.com <jorge.reales@dialapplet.com>
2023-06-23 21:56:43 -0500

    Feat: Add stars to outstanding product

--- src/app/products/view/components/productCard.tsx
+++ src/app/products/view/components/productCard.tsx
@@ -32,6 +32,16 @@ export default function ProductCard({imagePath,name,price,rating,section,offer}:
    {section === 'Offers' && <div className="absolute  rounded top-[-10px]  left-2 w-fit p-2 bg-amber-400 bg-opacity-90 text-red-900 animate-bounce">
         <span className="text-stroke-black text-2xl font-black">{offer}%</span>
     </div>} 
+
+    {section === 'outstanding' &&(<> <div className="absolute  rounded bottom-40 mb-1 right-5 w-fit p-2 text-red-900 z-10">
+        <span className="text-stroke-black text-md font-black ">{rating}</span>
+    </div>
+    
+    <div className="absolute  rounded bottom-40 right-2 w-fit p-2 text-red-900 ">
+    <FontAwesomeIcon icon={solidStar}  size={'2xl'}  color={'#EEB226'}/>
+</div>
+</>
+    )} 
     <img src={imagePath} className='h-3/5 hover:absolute hover:h-96 hover:w-72 hover:translate-x-[-10px] hover:border-2 hover:border-cyan-900 hover:translate-y-[-5px] hover:z-10'/>
     <p className="font-bold">{name}</p>
     <p>{section === 'Offers'&& <span className="line-through">${price}</span>} ${discount}</p>
